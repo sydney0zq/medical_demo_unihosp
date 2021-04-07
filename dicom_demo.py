fix the normalization issue

dicom_demo.py
@@ -10,17 +10,17 @@ HU_keptwin = np.array([-1250, 250])
 train_spacing = 5
 
 # demo 1: load dicom data and visualize the images
-if False:
+if True:
     visual_dir = "visual_videos"
     os.makedirs(visual_dir, exist_ok=True)
-    visualize_dicom_as_video("dicom_data/covid-1", output_fn=os.path.join(visual_dir, "covid-1.avi"))
-    visualize_dicom_as_video("dicom_data/covid-2", output_fn=os.path.join(visual_dir, "covid-2.avi"))
-    visualize_dicom_as_video("dicom_data/normal-1", output_fn=os.path.join(visual_dir, "normal-1.avi"))
-    visualize_dicom_as_video("dicom_data/normal-2", output_fn=os.path.join(visual_dir, "normal-2.avi"))
+    visualize_dicom_as_video("dicom_data/covid-1", output_fn=os.path.join(visual_dir, "covid-1.avi"), HU_keptwin=HU_keptwin)
+    visualize_dicom_as_video("dicom_data/covid-2", output_fn=os.path.join(visual_dir, "covid-2.avi"), HU_keptwin=HU_keptwin)
+    visualize_dicom_as_video("dicom_data/normal-1", output_fn=os.path.join(visual_dir, "normal-1.avi"), HU_keptwin=HU_keptwin)
+    visualize_dicom_as_video("dicom_data/normal-2", output_fn=os.path.join(visual_dir, "normal-2.avi"), HU_keptwin=HU_keptwin)
 
 # demo 2: normalize the images，from 16bit to 8bit
 # take the sample normal-1 as an example
-if True:
+if False:
     dicom_images, dicom_spacing = load_16bit_dicom_images("dicom_data/covid-1")
     print("dicom images.shape/.dtype {}/{}, dicom_spacing {}".format(dicom_images.shape, dicom_images.dtype, dicom_spacing))
 

medical_lib.py
@@ -140,11 +140,12 @@ def norm_16bit_to_8bit(dicom_16bit_imgs, HU_keptwin=None, bound_values=[0, 1]):
     dicom_8bit_imgs = (dicom_16bit_imgs*255).astype('uint8')
     return dicom_8bit_imgs
 
-def visualize_dicom_as_video(path="", output_fn="", dicom_images=None, fps=5, w_index=True):
+def visualize_dicom_as_video(path="", output_fn="", dicom_images=None, HU_keptwin=None):
     assert output_fn.endswith('avi'), "The output filename must be .avi suffix..."
     if dicom_images is None:
         dicom_images, spacing = load_16bit_dicom_images(path)
-        min_value, max_value = np.min(dicom_images), np.max(dicom_images)
+        min_value, max_value = HU_keptwin
+        dicom_images = np.clip(dicom_images, min_value, max_value)
         norm_images = (dicom_images - min_value) / (max_value - min_value)
         norm_images = np.uint8(norm_images * 255)
     else: